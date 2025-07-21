4d09d541a2c5373877deedfd1680971ac41608a5
Nishanth Reddy <nishanth.reddym004@gmail.com>
2025-07-21 12:18:00 +0530

    requirements added

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask,request,render_template,session
-import requests
+from time import perf_counter
 import re
+import aiohttp
+import asyncio
 
 class String(str):
       def Clean(self):
@@ -10,25 +12,42 @@ class String(str):
 
 app = Flask(__name__)
 
+def format_data(data):
+        cleaned_data = String(data).Clean().split(',')
+        formated_data = []
+        for item in cleaned_data:
+            if item and item not in formated_data and item.strip():
+                  formated_data.append(item)
+        return formated_data
+
 @app.route("/",methods=["GET","POST"])
 def home():
-        validatedData = {}
         if "verify" in request.form:
-                data = String(request.form['sno']).Clean().split(',')
-                for sno in data:
-                        validatedData[sno] = ValidateDCR(sno)
-                return render_template("home.html",validatedData = validatedData)
+                process = {}
+                start = perf_counter()
+                data = request.form['sno']
+                formated_data = format_data(data)
+                process['no_of_inputs'] = len(formated_data)
+                validatedData = asyncio.run(ValidateAllDCRs(formated_data))
+                end = perf_counter()
+                process['execution_time'] = f'{end-start:.3f}'
+                return render_template("home.html",validatedData = validatedData, process = process)
         return render_template("home.html")
 
-def ValidateDCR(modSrNum):
-    url = f'https://solardcrportal.nise.res.in/VerifyDCR/PnlNumberO?PnlNumber={modSrNum}'
-    try:
-        response = requests.get(url).text
-    except Exception as e:
+async def ValidateDCR(session,serialno):
+       url = f'https://solardcrportal.nise.res.in/VerifyDCR/PnlNumberO?PnlNumber={serialno}'
+       try:
+             async with session.get(url, timeout=10) as response:
+                    text = await response.text()
+                    return serialno, ("ItemTransId" in text)
+       except Exception as e:
         print(e.__str__)
-    if "ItemTransId" in response: result = True
-    else: result = False
-    return result
+
+async def ValidateAllDCRs(serialnos):
+       async with aiohttp.ClientSession() as session:
+        tasks = [ValidateDCR(session, sno) for sno in serialnos]
+        results = await asyncio.gather(*tasks)
+        return dict(results)
 
 """
 Test data:
